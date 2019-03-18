b41887c041cf9ba4ea7257c81c613d21df22347f
pair <huphup_1@126.com>
2019-03-18 17:09:40 +0800

    Initial commit

---
+++ src/components/layouts.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="container">
+        <!-- 左边的容器 -->
+        <section class="menu">
+        </section>
+        <!-- 右边的容器 -->
+        <section class="menu-content">
+        </section>
+    </section>
+</template>
+
+<script>
+    import menus from './menus';
+    export default {
+        components:{
+            menus
+        }
+    }
+</script>
+
+<style lang="less">
+
+</style>
+
---
+++ src/components/menus.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="list-todos">
+        <a class="list-todo activeListClass list" v-for="item in items" :key="item.id">
+            <span class="icon-lock" v-if="item.locked"></span><!--锁定图标-->
+            <span class="count-list" v-if="item.count">
+                {{item.count}}</span><!--数字 -->
+                {{item.title}}
+        </a>
+        <a class="link-list-new">
+            <span class="icon-plus"></span>
+            新增
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            items:[{title:'星期一',count:1,locked:true},
+            {title:'星期二',count:2,locked:true},
+            {title:'星期三',count:3,locked:false}]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
